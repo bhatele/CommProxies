9fa78013ae6e5511cfb00e667f132f7e10e165b1
Edgar Solomonik <solomon2@illinois.edu>
2009-10-15 20:21:11 +0000

    Finished first bare bones version that simulates a  very simple pattern where numpatches = numpes

--- namdcomm/namdcomm.C
+++ namdcomm/namdcomm.C
@@ -8,35 +8,59 @@ using namespace std;
 #define MAX_ITER        25
 
 #define NUM_PATCHES	6
-#define MSG_SIZE	1000
+#define MSG_SIZE	2000
 #define COMPUTES_PER_P	9
 
 //determine processor locations of computes to which any given processor will send data
 void determineDest(int myRank, int numDest, int numPes, int numPatches, int numComputes, int pComputes, int *destinations){
-  int startPe = numPes - (((numPatches/numPes*pComputes)*myRank + (numPatches/numPes+1)*min(myRank, numPatches % numPes))%numPes);
+  //int startPe = numPes - (((numPatches/numPes*pComputes)*myRank + (numPatches/numPes+1)*min(myRank, numPatches % numPes))%numPes);
+  int startPe = (myRank + numPes - numDest/2)%numPes;
   int pe = startPe;
   for (int i = 0; i < numDest; i++){
     destinations[i] = pe;
-    pe--;
-    if (pe == -1) pe = numPes-1;
+    pe++;
+    if (pe == numPes) pe = 0;
   }
 }
 
-void sendandreceive(int myRank, int numDest, int numPes, int numPatches, int numComputes, int pComputes, int *destinations, int msgSize, int myComputes){
-    /*MPI_Irecv(right_edge_in, blockDimX, MPI_DOUBLE, calc_pe(myRow, wrap_y(myCol+1)), RIGHT, MPI_COMM_WORLD, &req[RIGHT-1]);
-    MPI_Irecv(left_edge_in, blockDimX, MPI_DOUBLE, calc_pe(myRow, wrap_y(myCol-1)), LEFT, MPI_COMM_WORLD, &req[LEFT-1]);
-    MPI_Irecv(&temperature[blockDimX+1][1], blockDimY, MPI_DOUBLE, calc_pe(wrap_x(myRow+1), myCol), BOTTOM, MPI_COMM_WORLD, &req[BOTTOM-1]);
-    MPI_Irecv(&temperature[0][1], blockDimY, MPI_DOUBLE, calc_pe(wrap_x(myRow-1), myCol), TOP, MPI_COMM_WORLD, &req[TOP-1]);
-
-
-    MPI_Send(left_edge_out, blockDimX, MPI_DOUBLE, calc_pe(myRow, wrap_y(myCol-1)), RIGHT, MPI_COMM_WORLD);
-    MPI_Send(right_edge_out, blockDimX, MPI_DOUBLE, calc_pe(myRow, wrap_y(myCol+1)), LEFT, MPI_COMM_WORLD);
-    MPI_Send(&temperature[1][1], blockDimY, MPI_DOUBLE, calc_pe(wrap_x(myRow-1), myCol), BOTTOM, MPI_COMM_WORLD);
-    MPI_Send(&temperature[blockDimX][1], blockDimY, MPI_DOUBLE, calc_pe(wrap_x(myRow+1), myCol), TOP, MPI_COMM_WORLD);
-
-    MPI_Waitall(4, req, status);
-*/
+double sendandreceive(int myRank, int numDest, int numPes, int *destinations, int msgSize, int myComputes, int **recvbuffer, int **sendbuffer, MPI_Request *req, MPI_Status *stat){
+  printf("sending with message size = %d\n", msgSize);
+  double sendtime = MPI_Wtime();
+  for ( int i = 0; i < numDest; i++){
+    MPI_Irecv(recvbuffer[i], msgSize, MPI_INT, destinations[i], myRank*numPes+destinations[i], MPI_COMM_WORLD, &req[i]);
+    MPI_Send(sendbuffer[i], msgSize, MPI_INT, destinations[i], destinations[i]*numPes + myRank, MPI_COMM_WORLD);
+  }
+  sendtime = MPI_Wtime() - sendtime;
+  MPI_Waitall(numDest, req, stat);
+  return sendtime;
 }
+
+double recvfirst(int myRank, int numDest, int numPes, int *destinations, int msgSize, int myComputes, int **recvbuffer, int **sendbuffer, MPI_Request *req, MPI_Status *stat){
+  for ( int i = 0; i < numDest; i++){
+    MPI_Irecv(recvbuffer[i], msgSize, MPI_INT, destinations[i], myRank*numPes+destinations[i], MPI_COMM_WORLD, &req[i]);
+  }
+  double sendtime = MPI_Wtime();
+  for (int i = 0; i < numDest; i++){
+    MPI_Send(sendbuffer[i], msgSize, MPI_INT, destinations[i], destinations[i]*numPes + myRank, MPI_COMM_WORLD);
+  }
+  sendtime = MPI_Wtime() - sendtime;
+  MPI_Waitall(numDest, req, stat);
+  return sendtime;
+}
+
+double sendfirst(int myRank, int numDest, int numPes, int *destinations, int msgSize, int myComputes, int **recvbuffer, int **sendbuffer, MPI_Request *req, MPI_Status *stat){
+  double sendtime = MPI_Wtime();
+  for (int i = 0; i < numDest; i++){
+    MPI_Send(sendbuffer[i], msgSize, MPI_INT, destinations[i], destinations[i]*numPes + myRank, MPI_COMM_WORLD);
+  }
+  sendtime = MPI_Wtime() - sendtime;
+  for ( int i = 0; i < numDest; i++){
+    MPI_Irecv(recvbuffer[i], msgSize, MPI_INT, destinations[i], myRank*numPes+destinations[i], MPI_COMM_WORLD, &req[i]);
+  }
+  MPI_Waitall(numDest, req, stat);
+  return sendtime;
+}
+
 
 int main(int argc, char **argv) {
   int myRank, numPes;
@@ -49,21 +73,20 @@ int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numPes);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-  MPI_Request req[4];
-  MPI_Status status[4];
-
+  
   if (argc > 1) numPatches = atoi(argv[1]);
   else numPatches = NUM_PATCHES;
 
-  if (argc > 2) msgSize = atoi(argv[2]);
-  else msgSize = MSG_SIZE;
-
-  if (argc > 3) pComputes = atoi(argv[3]);
+  if (argc > 2) pComputes = atoi(argv[2]);
   else pComputes = COMPUTES_PER_P;
+  
+  if (argc > 3) msgSize = atoi(argv[3]);
+  else msgSize = MSG_SIZE;
   
   if (argc > 4) iterations = atoi(argv[4]);
   else iterations = MAX_ITER;
 
+  numPatches = numPes;
   numComputes = pComputes*numPatches;
 
   if (myRank == 0){
@@ -89,19 +112,53 @@ int main(int argc, char **argv) {
   
   numDest = myPatches*pComputes;
   destinations = new int[myPatches*pComputes];
+  MPI_Request *req = new MPI_Request[numDest];
+  MPI_Status *stat = new MPI_Status[numDest];
   
   //determine locations of computes  
   determineDest(myRank, numDest, numPes, numPatches, numComputes, pComputes, destinations);
+  int **recvbuffer = new int*[numDest];
+  int **sendbuffer = new int*[numDest];
+  for ( int i = 0; i < numDest; i++){
+    sendbuffer[i] = new int[msgSize];
+    recvbuffer[i] = new int[msgSize];
+  }
 
   double startTimer = MPI_Wtime();
+  double sendTime = 0;
+  /*if (myRank == 0) printf("Doing loop Irecv Send loop++ iterations\n");
   for (currIter = 0; currIter < iterations; currIter++){
-    sendandreceive(myRank, numDest, numPes, numPatches, numComputes, pComputes, destinations, msgSize, myComputes);
+    sendTime += sendandreceive(myRank, numDest, numPes, destinations, msgSize, myComputes, recvbuffer, sendbuffer, req, stat);
     if (myRank == 0 && currIter % 20 == 0){
-      printf("%d iterations elapsed, last 20 iterations took %llf seconds\n", currIter, MPI_Wtime() - startTimer);
+      printf("%d iterations elapsed, last 20 iterations took %lf seconds, with a total sendTime of %lf\n", currIter, MPI_Wtime() - startTimer, sendTime);
       startTimer = MPI_Wtime();
+      sendTime = 0;
+    }
+  }*/
+  
+  startTimer = MPI_Wtime();
+  sendTime = 0;
+  if (myRank == 0) printf("Doing recvfirst iterations\n");
+  for (currIter = 0; currIter < iterations; currIter++){
+    sendTime += recvfirst(myRank, numDest, numPes, destinations, msgSize, myComputes, recvbuffer, sendbuffer, req, stat);
+    if (myRank == 0 && currIter % 20 == 0){
+      printf("%d iterations elapsed, last 20 iterations took %lf seconds, with a total sendTime of %lf\n", currIter, MPI_Wtime() - startTimer, sendTime);
+      startTimer = MPI_Wtime();
+      sendTime = 0;
     }
   }
 
+  /*startTimer = MPI_Wtime();
+  sendTime = 0;
+  if (myRank == 0) printf("Doing sendfirst iterations\n");
+  for (currIter = 0; currIter < iterations; currIter++){
+    sendTime += sendfirst(myRank, numDest, numPes, destinations, msgSize, myComputes, recvbuffer, sendbuffer, req, stat);
+    if (myRank == 0 && currIter % 20 == 0){
+      printf("%d iterations elapsed, last 20 iterations took %lf seconds, with a total sendTime of %lf\n", currIter, MPI_Wtime() - startTimer, sendTime);
+      startTimer = MPI_Wtime();
+      sendTime = 0;
+    }
+  }*/
 
   MPI_Finalize();
   return 0;
